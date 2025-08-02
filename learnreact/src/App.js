e5f54dc9b681460ce2c12c62119390601abecd41
Vrushali A Poojary <vrushaliash04@gmail.com>
2025-08-03 01:16:54 +0530

    learnreact

--- learnreact/src/App.js
+++ learnreact/src/App.js
@@ -7,21 +7,9 @@ import React, { useState } from 'react';
 
 function App() {
   const initialProducts = [
-    {
-      price: 1000,
-      name: "Product 1",
-      quantity: 0
-    },
-    {
-      price: 100,
-      name: "Product 2",
-      quantity: 0
-    },
-    {
-      price: 10,
-      name: "Product 3",
-      quantity: 0
-    }
+    { price: 1000, name: "Product 1", quantity: 0 },
+    { price: 100, name: "Product 2", quantity: 0 },
+    { price: 10, name: "Product 3", quantity: 0 }
   ];
 
   const [productList, setProductList] = useState(initialProducts);
@@ -32,30 +20,32 @@ function App() {
     let newTotalAmount = totalAmount;
     newProductList[index].quantity++;
     newTotalAmount += newProductList[index].price;
-    setotalAmount(newTotalAmount)
+    setotalAmount(newTotalAmount);
     setProductList(newProductList);
   };
+
   const decrementQuantity = (index) => {
     let newProductList = [...productList];
     let newTotalAmount = totalAmount;
-    if(newProductList[index].quantity > 0 ){
+    if (newProductList[index].quantity > 0) {
       newProductList[index].quantity--;
-      newProductList[index].quantity -= newProductList[index].quantity = 0;}  
-    setotalAmount(newTotalAmount)
+      newTotalAmount -= newProductList[index].price;
+    }
+    setotalAmount(newTotalAmount);
     setProductList(newProductList);
   };
 
   return (
     <>
       <Navbar />
-      <main className="container mt-5"> 
-        <ProductList 
-          productList={productList} 
-          incrementQuantity={incrementQuantity} 
+      <main className="container mt-5">
+        <ProductList
+          productList={productList}
+          incrementQuantity={incrementQuantity}
           decrementQuantity={decrementQuantity}
         />
       </main>
-      <Footer  totalAmount={totalAmount} /> 
+      <Footer totalAmount={totalAmount} />
     </>
   );
 }
