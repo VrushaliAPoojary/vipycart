f56181e1e1ab53c789cf03d01e2ef908ad78f5b9
Vrushali A Poojary <vrushaliash04@gmail.com>
2025-08-02 20:54:01 +0530

    learnreact

--- learnreact/src/App.js
+++ learnreact/src/App.js
@@ -1,9 +1,32 @@
 import logo from './logo.svg';
 import './App.css';
-
+import Navbar from './components/Navbar.js';
+import ProductList from './components/ProductList.js';
+import Footer from './components/Footer.js'
 function App() {
+  const productList = [
+    {
+      price: 99999,
+      name: "Product 1",
+      quantity: 0
+    },
+    {
+      price: 9999,
+      name: "Product 2",
+      quantity: 0
+    },
+    {
+      price: 999,
+      name: "Product 3",
+      quantity: 0
+    }
+    
+  ]
   return (
     <>
+    <Navbar />
+    <ProductList productList={productList}/>
+   {/* <Footer />*/}
     </>
   );
 }
---
+++ learnreact/src/components/Navbar.js
@@ -0,0 +1,25 @@
+
+import React, { Component } from "react";
+
+
+
+class Navbar extends Component {
+  state = {};
+  render() {
+    return (
+     <nav className="navbar navbar-expand-lg bg-primary">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="#">
+            vipy_cart
+          </a>
+         
+          
+           
+          
+        </div>
+      </nav>
+    );
+  }
+}
+
+export default Navbar;
