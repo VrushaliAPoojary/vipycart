5e992a4ab947c9e1d760f26e62207ae202251c71
VrushaliAPoojary <vrushaliash04@gmail.com>
2026-03-05 22:05:18 +0530

    node

--- learnNODE/fsmodule.js
+++ learnNODE/fsmodule.js
@@ -4,4 +4,10 @@ fs.readFile('file.txt', 'utf8', (err, data)=>{
     console.log(err, data)
 })
 
+const a = fs.readFileSync('file.txt')  //intensionally blocking
+const b = fs.writeFileSync("file.txt", "hii appu", ()=>{
+    console.log("runn!!!!!!")
+})
+
 console.log("finishhh")
+console.log(a.toString())
