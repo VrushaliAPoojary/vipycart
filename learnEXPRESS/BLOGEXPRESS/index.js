d91514d5f5c435238c2f1e1959149786d0e61251
VrushaliAPoojary <vrushaliash04@gmail.com>
2026-03-09 00:16:29 +0530

    express

--- learnEXPRESS/BLOGEXPRESS/index.js
+++ learnEXPRESS/BLOGEXPRESS/index.js
@@ -2,8 +2,10 @@ const express = require('express')
 const path = require('path')
 const app = express()
 const port = 3000
+const { engine } =require('express-handlebars');
 
-
+app.engine('handlebars', engine());
+app.set('view engine', 'handlebars');
 
 app.use(express.static(path.join(__dirname, "static")))
 app.use('/', require(path.join(__dirname, 'routes/blog.js')))
--- learnEXPRESS/BLOGEXPRESS/routes/blog.js
+++ learnEXPRESS/BLOGEXPRESS/routes/blog.js
@@ -4,7 +4,8 @@ const blogs = require('../data/blogs')
 const router = express.Router()
 
 router.get('/', (req, res)=>{
-    res.sendFile(path.join(__dirname,'../templates/index.html'))
+   // res.sendFile(path.join(__dirname,'../templates/index.html'))
+   res.render('home')
 })
 router.get('/blog', (req, res)=>{
     // blogs.forEach((e) =>{
